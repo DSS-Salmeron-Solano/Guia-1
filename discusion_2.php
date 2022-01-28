ecdfd4cc01fa686fd2e51b68dc95fc5f7e042db5
DanielSalmeron3b <danielesajr@gmail.com>
2022-01-27 19:00:06 -0600

    🚀 Primer Commit GUIA1

---
+++ discusion_2.php
@@ -0,0 +1,38 @@
+<?php
+
+echo <<<NOMBRES
+<p>
+    Daniel Eduardo
+</p>
+NOMBRES;
+
+echo <<<APELLIDOS
+<p>
+    Salmeron Alvarado
+</p>
+APELLIDOS;
+
+echo <<<LUGAR_NACIMIENTO
+<p>
+    San Salvador, El Salvador
+</p>
+LUGAR_NACIMIENTO;
+
+echo <<<EDAD
+<p>
+    18
+</p>
+EDAD;
+
+echo <<<CARNET
+<p>
+    SA212548
+</p>
+CARNET;
+
+
+
+
+
+
+
